8e06d1966355eda51eadb139f76522d82a2b35f3
Ilan Guenet <ilan.guenet@epita.fr>
2020-05-24 22:58:09 +0200

    [ADD][OPTION] To run the program with a specific csv file. You can use the -f options by the following way ./shuffle_me -f filename.csv

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <string>
 
 #include "main-window.hh"
 
@@ -7,7 +8,11 @@ int main(int argc, char **argv)
     QApplication app (argc, argv); // create the app
 
     Data<WordsManager::pair_t>* data = new Data<WordsManager::pair_t>;
-    WordsManager* words_manager = new WordsManager(data);
+    WordsManager* words_manager = nullptr;
+    if (argc == 3 && std::string(argv[1]) == "-f")
+        words_manager = new WordsManager(data, argv[2]);
+    else
+        words_manager = new WordsManager(data);
     MainWindow main_window(words_manager);
     main_window.show();
     return app.exec(); // execute the app
